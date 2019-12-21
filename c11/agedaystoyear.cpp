0ebf3255fa1bb91b9a4f6f91196da2a58116a51f
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-12-21 18:09:40 +0530

    agedaystoyear.cpp

---
+++ c11/agedaystoyear.cpp
@@ -0,0 +1,17 @@
+//Program to convert age of a person from days to year month and days format.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        int d, year, month, day;
+        cout << "Enter age(in no of days) : ";
+        cin >> d;
+        year = d / 365.25;
+        d = d - (year * 365.25);
+        month = d / 30.5;
+        days = d - (int)(month * 30.5);
+        return 0;
+}
